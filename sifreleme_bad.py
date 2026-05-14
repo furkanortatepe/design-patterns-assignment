28f88d3c41038dfc9a1edab96b53ec9d657fe2df
Furkan <furkanortatepe31@gmail.com>
2026-05-14 13:22:48 +0300

    Add SifrelemeAraci class for text encryption

--- sifreleme_bad.py
+++ sifreleme_bad.py
@@ -1,1 +1,26 @@
-
+class SifrelemeAraci:
+    def __init__(self, algoritma):
+        if algoritma == "basit":
+            self.algoritma = "basit"
+        elif algoritma == "ters":
+            self.algoritma = "ters"
+        else:
+            self.algoritma = "basit"
+    
+    def sifrele(self, metin):
+        if self.algoritma == "basit":
+            sonuc = ""
+            for harf in metin:
+                sonuc += chr(ord(harf) + 1)
+            return sonuc
+        elif self.algoritma == "ters":
+            return metin[::-1]
+    
+    def coz(self, sifreli):
+        if self.algoritma == "basit":
+            sonuc = ""
+            for harf in sifreli:
+                sonuc += chr(ord(harf) - 1)
+            return sonuc
+        elif self.algoritma == "ters":
+            return sifreli[::-1]
